13297814b364888ec9d9da372b7a04618f2bb275
cameron1024 <cameron@studdstreet.com>
2021-08-05 23:33:42 +0100

    Update flatter.rb

--- Formula/flatter.rb
+++ Formula/flatter.rb
@@ -4,3 +4,8 @@ class Flatter < Formula
   url "https://github.com/cameron1024/flatter/releases/download/v0.1.0/flatter-mac.tar.gz"
   sha256 "53d1758ab79e2196bf4ad9668cdc18d7fa1c7f5406fd794a89ac55b650431041"
   version "0.1.0"
+  
+  def install
+    bin.install "flatter"
+  end
+end
