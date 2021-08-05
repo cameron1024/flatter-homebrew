307da97e0063cf03ad7489df8eb1d17110987206
cameron1024 <cameron@studdstreet.com>
2021-08-05 23:33:09 +0100

    Update flatter.rb

--- Formula/flatter.rb
+++ Formula/flatter.rb
@@ -2,5 +2,5 @@ class Flatter < Formula
   desc "Flatter SVG to PNG renderer"
   homepage "http://github.com/cameron1024/flatter"
   url "https://github.com/cameron1024/flatter/releases/download/v0.1.0/flatter-mac.tar.gz"
-  sha256
+  sha256 "53d1758ab79e2196bf4ad9668cdc18d7fa1c7f5406fd794a89ac55b650431041"
   version "0.1.0"
